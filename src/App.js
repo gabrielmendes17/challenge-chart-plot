0299eaecdcba734050089f8233cfbf0f26da6f85
Gabriel <gabrielmendes17@gmail.com>
2019-11-24 14:53:27 -0300

    Implementando event-data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import LineChart from "./components/LineChart";
 
 function App() {
-  const event = [
+  const events = [
     {
       type: "start",
       timestamp: 1519862400000,
@@ -19,15 +19,72 @@ function App() {
     {
       type: "data",
       timestamp: 1519862400000,
-      os: "linux",
-      browser: "chrome",
+      os: "Linux",
+      browser: "Chrome",
       min_response_time: 0.1,
       max_response_time: 1.3
-    }
+    },
+    {
+      type: "data",
+      timestamp: 1519862400000,
+      os: "Mac",
+      browser: "Chrome",
+      min_response_time: 0.2,
+      max_response_time: 1.2
+    },
+    {
+      type: "data",
+      timestamp: 1519862400000,
+      os: "Mac",
+      browser: "Firefox",
+      min_response_time: 0.3,
+      max_response_time: 1.2
+    },
+    {
+      type: "data",
+      timestamp: 1519862400000,
+      os: "Linux",
+      browser: "Firefox",
+      min_response_time: 0.1,
+      max_response_time: 1.0
+    },
+    {
+      type: "data",
+      timestamp: 1519862460000,
+      os: "Linux",
+      browser: "Chrome",
+      min_response_time: 0.2,
+      max_response_time: 0.9
+    },
+    {
+      type: "data",
+      timestamp: 1519862460000,
+      os: "Mac",
+      browser: "Chrome",
+      min_response_time: 0.1,
+      max_response_time: 1.0
+    },
+    {
+      type: "data",
+      timestamp: 1519862460000,
+      os: "Mac",
+      browser: "Firefox",
+      min_response_time: 0.2,
+      max_response_time: 1.1
+    },
+    {
+      type: "data",
+      timestamp: 1519862460000,
+      os: "Linux",
+      browser: "Firefox",
+      min_response_time: 0.3,
+      max_response_time: 1.4
+    },
+    { type: "stop", timestamp: 1519862460000 }
   ];
   return (
     <div className="App">
-      <LineChart serie={"series-1"} event={event} handleEvent={event} />
+      <LineChart serie={"series-1"} events={events} />
     </div>
   );
 }
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -19,19 +19,19 @@ class LineChart extends Component {
         {
           name: this.props.serie,
           data: ["0.1", "0.2"]
-        },
-        {
-          name: this.props.serie,
-          data: ["0.3", "0.5"]
-        },
-        {
-          name: this.props.serie,
-          data: ["1.1", "1.3"]
-        },
-        {
-          name: this.props.serie,
-          data: ["1.3", "1.5"]
         }
+        // {
+        //   name: this.props.serie,
+        //   data: ["0.3", "0.5"]
+        // },
+        // {
+        //   name: this.props.serie,
+        //   data: ["1.1", "1.3"]
+        // },
+        // {
+        //   name: this.props.serie,
+        //   data: ["1.3", "1.5"]
+        // }
       ]
     };
   }
@@ -53,8 +53,46 @@ class LineChart extends Component {
     });
   };
 
-  handleDataEvent = item => {
-    console.log("data event", item);
+  handleDataEvents = itens => {
+    console.log("datas events", itens);
+    itens.map((item, index, arr) => {
+      console.log(index, arr);
+      const secondItem = itens.find(
+        i =>
+          i.os === item.os &&
+          i.browser === item.browser &&
+          i.timestamp !== item.timestamp
+      );
+      console.log(item, secondItem);
+      if (secondItem) {
+        itens.splice(index, 1);
+        return this.transformItensTochartLine(item, secondItem);
+      } else {
+        return itens.splice(index, 1);
+      }
+    });
+  };
+
+  getResponseTimes = (item_res, second_item_res) => {
+    return item_res > second_item_res
+      ? { item_res, second_item_res }
+      : { item_res, second_item_res };
+  };
+
+  transformItensTochartLine = (item, secondItem) => {
+    let { item_res, second_item_res } = this.getResponseTimes(
+      item.min_response_time,
+      secondItem.min_response_time
+    );
+    const newSerieMin = {
+      name: `${item.os} ${item.browser} Min Response Time`,
+      data: [item_res, second_item_res]
+    };
+    console.log(this.state);
+    this.setState({
+      series: [...this.state.series, newSerieMin]
+    });
+    console.log(this.state);
   };
 
   handleStopEvent = item => {
@@ -62,30 +100,26 @@ class LineChart extends Component {
   };
 
   getSecondsBtweenTwoDates = ({ begin, end }) => {
-    const now = moment(new Date(begin)); //todays date
-    const then = moment(new Date(end));
-    const duration = moment.duration(then.diff(now));
+    const duration = moment.duration(
+      moment(new Date(end)).diff(moment(new Date(begin)))
+    );
     return moment(new Date(duration)).format("mm:ss");
   };
 
-  handleChartChange = event => {
-    this.props.event.forEach(item => {
-      switch (item.type) {
-        case "start":
-          this.handleStartEvent(item);
-          break;
-        case "span":
-          this.handleSpanEvent(item);
-          break;
-        case "data":
-          this.handleDataEvent(item);
-          break;
-        case "stop":
-          this.handleStopEvent(item);
-          break;
-        default:
-      }
-    });
+  separateEvents = events => {
+    const start = this.props.events.find(i => i.type === "start");
+    const stop = this.props.events.find(i => i.type === "stop");
+    const span = this.props.events.find(i => i.type === "span");
+    const datas = this.props.events.filter(i => i.type === "data");
+    return { start, stop, span, datas };
+  };
+
+  handleChartChange = events => {
+    const { start, stop, span, datas } = this.separateEvents(events);
+    this.handleStartEvent(start);
+    this.handleSpanEvent(span);
+    this.handleStopEvent(stop);
+    this.handleDataEvents(datas);
   };
 
   componentDidMount = () => {
